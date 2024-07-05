25cebbbb86d45781ba096d6842d73d070b8a0c9e
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-07-05 15:32:53 +0200

    creazione form

--- resources/views/Comics/create.blade.php
+++ resources/views/Comics/create.blade.php
@@ -11,6 +11,42 @@
         <div class="row">
             <div class="col-12">
 
+                <form method="POST" action="">
+                    @csrf
+                    <div class="mb-3">
+                        <label class="form-label">Titolo</label>
+                        <input type="text" class="form-control" name="title" required>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Descrizione</label>
+                        <textarea type="text" class="form-control" name="description" required></textarea>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Percorso immagione</label>
+                        <input type="text" class="form-control" name="thumb" required>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Prezzo</label>
+                        <input type="number" min="0.99" max="999.99" class="form-control" name="price" required>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Serie</label>
+                        <input type="text" class="form-control" name="series" required>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Data di uscita</label>
+                        <input type="number" min="1980" max="2024" class="form-control" name="sale_date" required>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Tipo</label>
+                        <input type="text" class="form-control" name="type" required>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Artista</label>
+                        <input type="number" min="0" max="10" class="form-control" name="artist" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
 
             </div>
         </div>
