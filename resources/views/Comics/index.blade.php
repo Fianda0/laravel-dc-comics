4585d452644d5b4b4cea2000a9cbb9a4c3f9a62a
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-07-05 16:19:57 +0200

    Aggiunta bottone modifica

--- resources/views/Comics/index.blade.php
+++ resources/views/Comics/index.blade.php
@@ -8,6 +8,7 @@
                     <p>{{ $comic->title }}</p>
                     <img src="{{ $comic->thumb }}" alt="">
                     <p>{{ $comic->description }}</p>
+                    <a href="{{ route('comics.edit', $comic->id) }}">Modifica</a>
                 </a>
             </div>
         @endforeach
